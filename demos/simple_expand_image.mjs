373e6be7e9c2da426b9839842907bc7754b57411
Raymond Camden <raymondcamden@gmail.com>
2024-08-08 12:40:36 -0500

    moar work

---
+++ demos/simple_expand_image.mjs
@@ -0,0 +1,80 @@
+import fs from 'fs';
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+
+async function getAccessToken(id, secret) {
+
+	const params = new URLSearchParams();
+
+	params.append('grant_type', 'client_credentials');
+	params.append('client_id', id);
+	params.append('client_secret', secret);
+	params.append('scope', 'openid,AdobeID,firefly_enterprise,firefly_api,ff_apis');
+	
+	let resp = await fetch('https://ims-na1.adobelogin.com/ims/token/v3', 
+		{ 
+			method: 'POST', 
+			body: params
+		}
+	);
+
+	return (await resp.json()).access_token;
+}
+
+async function uploadImage(filePath, fileType, id, token) {
+
+	let stream = fs.createReadStream(filePath);
+	let stats = fs.statSync(filePath);
+	let fileSizeInBytes = stats.size;
+
+	let upload = await fetch('https://firefly-api.adobe.io/v2/storage/image', {
+		method:'POST', 
+		headers: {
+			'Authorization':`Bearer ${token}`, 
+			'X-API-Key':id, 
+			'Content-Type':fileType, 
+			'Content-Length':fileSizeInBytes
+		}, 
+		duplex:'half', 
+		body:stream
+	});
+
+	return await upload.json();
+}
+
+async function expandImage(source, id, token) {
+
+	let body = {
+		image: {
+			source: {
+				uploadId: source
+			}
+		},
+		size: {
+			width: 3999,
+			height: 2048
+		}
+	}
+
+	let req = await fetch('https://firefly-api.adobe.io/v3/images/expand', {
+		method:'POST',
+		headers: {
+			'X-Api-Key':id, 
+			'Authorization':`Bearer ${token}`,
+			'Content-Type':'application/json'
+		}, 
+		body: JSON.stringify(body)
+	});
+
+	return await req.json();
+}
+
+let token = await getAccessToken(CLIENT_ID, CLIENT_SECRET);
+
+let imageAsset = await uploadImage('./bike.jpg', 'image/jpeg', CLIENT_ID, token);
+console.log('Image resut', JSON.stringify(imageAsset,null,'\t'));
+
+let result = await expandImage(imageAsset.images[0].id, CLIENT_ID, token);
+
+console.log(JSON.stringify(result,null,'\t'));
---
+++ labs/developer/ex1/generate3.mjs
@@ -0,0 +1,69 @@
+import fs from 'fs';
+import { Readable } from 'stream';
+import { finished } from 'stream/promises';
+import slugify from '@sindresorhus/slugify';
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+
+async function getAccessToken(id, secret) {
+
+	const params = new URLSearchParams();
+
+	params.append('grant_type', 'client_credentials');
+	params.append('client_id', id);
+	params.append('client_secret', secret);
+	params.append('scope', 'openid,AdobeID,firefly_enterprise,firefly_api,ff_apis');
+	
+	let resp = await fetch('https://ims-na1.adobelogin.com/ims/token/v3', 
+		{ 
+			method: 'POST', 
+			body: params
+		}
+	);
+
+	return (await resp.json()).access_token;
+}
+
+async function textToImage(prompt, id, token) {
+
+	let body = {
+		numVariations:1, 
+		prompt
+	}
+
+	let req = await fetch('https://firefly-api.adobe.io/v3/images/generate', {
+		method:'POST',
+		headers: {
+			'X-Api-Key':id, 
+			'Authorization':`Bearer ${token}`,
+			'Content-Type':'application/json'
+		}, 
+		body: JSON.stringify(body)
+	});
+
+	return await req.json();
+}
+
+async function downloadFile(url, filePath) {
+	let res = await fetch(url);
+	const body = Readable.fromWeb(res.body);
+	const download_write_stream = fs.createWriteStream(filePath);
+	return await finished(body.pipe(download_write_stream));
+}
+
+let prompts = fs.readFileSync('./input.txt','utf8').trim().split('\n');
+
+for(let prompt of prompts) {
+	console.log(`Generating an image based on the prompt: ${prompt}`);
+
+	let token = await getAccessToken(CLIENT_ID, CLIENT_SECRET);
+
+	let result = await textToImage(prompt, CLIENT_ID, token);
+
+	for(let output of result.outputs) {
+		let fileName = `./${slugify(prompt)}-${output.seed}.jpg`;
+		await downloadFile(output.image.url, fileName);
+		console.log(`Saved ${fileName}`);
+	}
+}
